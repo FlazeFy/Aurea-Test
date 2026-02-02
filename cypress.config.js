4014e0f987d29a6db6443da1f53b4579b228fe9f
FlazeFy <79736302+FlazeFy@users.noreply.github.com>
2026-02-02 16:52:16 +0700

    test(auth): add integration tests for POST login API (2 success, 4 failure cases)
    Create a command template to assert that certain keys exist in an object, and to assert the default response properties

--- cypress.config.js
+++ cypress.config.js
@@ -3,14 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
         // baseUrl : 'https://aurea.leonardhors.com',
-
         // For Backend Apps
         baseUrl: 'http://localhost:5555/',
-
         // For Frontend Apps
         // baseUrl: 'http://localhost:3000/',
-        
-        specPattern : "support",
-        supportFile : false,
+        specPattern : ["integrations"],
+        supportFile: "supports/e2e.ts"    
     }
 })
